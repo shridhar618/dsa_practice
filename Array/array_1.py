8b1bf2f84ece77c287f7dae4c9a66c841a0d5253
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-05-15 19:59:44 +0530

    init

--- Array/array_1.py
+++ Array/array_1.py
@@ -1,9 +1,32 @@
-def first_recurring_no(nums):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):   
-            if nums[i] == nums[j]:
-                return nums[i]
-    return None                            
+# def first_recurring_no(nums):
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):   
+#             if nums[i] == nums[j]:
+#                 return nums[i]
+#     return None                            
 
-nums = [2, 5, 3, 5, 2]
-print(first_recurring_no(nums))  
+# nums = [2, 5, 3, 5, 2]
+# print(first_recurring_no(nums))  
+
+
+
+# def first_recurring_no_2(nums):
+#     left=0
+#     right=len(nums)-1
+    
+#     for i in range(len(nums)):
+#         if nums[left]==nums[right]:
+#             return nums[left]
+
+
+
+def first_recurring_no_2(nums):
+    freq={}
+    for num in nums:
+        if num in freq:
+            return num
+        freq[num]=1
+    
+nums=[2,4,3,5,2,3,8,7]
+result=first_recurring_no_2(nums)
+print(result)
