c95998dced4c984d44639bdbdd1ae2dc6186b78e
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-05-17 11:42:56 +0530

    ...

--- Array/bubble_sort.py
+++
@@ -1,9 +0,0 @@
-def sort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr)-1):
-            if arr[j]>arr[j+1]:
-                arr[j],arr[j+1]=arr[j+1],arr[j]
-    return arr
-            
-arr=[4,5,3,2,1,9,3]
-print(sort(arr))
